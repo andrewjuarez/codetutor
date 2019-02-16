76cdb36d4dc9c1c44acffba955e16f41a9e65b5f
kevinnguyen125 <kevinnguyen125@gmail.com>
2019-02-16 02:23:22 -0800

    Basic Landing Page

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
 
-export default class App extends Component {
-  state = { username: null };
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Navbar from './Components/Navbar';
+
+import Background from './bg.png';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  }
+});
+
+
+class App extends Component {
+  state = {};
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    console.log('TEST');
   }
 
   render() {
-    const { username } = this.state;
+    const { classes } = this.props;
+
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
+      <Grid container style={{ height: '100%', border: 'blue solid 4px' }}>
+        <Grid item style={{ position: 'absolute', width: '100%', border: 'yellow solid 4px' }}>
+          <Navbar />
+        </Grid>
+        <Grid container style={{ border: 'red solid 4px', backgroundImage: `url(${Background})`, backgroundSize: 'cover' }} justify="center" alignItems="center">
+          <Grid container justify="center" style={{ height: '10%' }}>
+            <Grid item xs={4}>
+              <Button fullWidth variant="contained" color="primary" size="large" className={classes.button} style={{ fontSize: 30 }}>
+                New Session
+              </Button>
+              <Grid style={{ display: 'flex', flexDirection: 'row', color: 'white' }}>
+                <hr style={{ width: 150, weight: 10 }} />
+                  OR
+                <hr style={{ width: 150, weight: 10 }} />
+              </Grid>
+              <Button fullWidth variant="contained" color="primary" size="large" className={classes.button} style={{ fontSize: 30 }}>
+                Join Session
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
+
+export default withStyles(styles)(App);
--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,33 @@
+// Material UI Theme
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33a095',
+      main: '#00897b',
+      dark: '#005f56',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#51b7ae',
+      main: '#26a69a',
+      dark: '#1a746b',
+      contrastText: '#fff',
+    },
+  },
+});
+
+function AppWithTheme() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(<AppWithTheme />, document.getElementById('root'));
