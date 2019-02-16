081a3a63f1b5cfc8d8b922bbf72075612de5e3cd
SahilRailkar <30790706+SahilRailkar@users.noreply.github.com>
2019-02-16 03:07:00 -0800

    Added compilation functionality in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 const os = require('os');
 const body_parser = require("body-parser");
+const request = require('request')
 
 const app = express();
 
@@ -21,3 +22,32 @@ const sessionAPIroutes = require("./api/session");
 app.use(sessionAPIroutes);
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+function compile(s, l, vI) {
+    const cI = '771d82a586d3f76e15e5c0e7d2d64bce'
+    const cS = '37939d9e0029dafe91ca0c5170059f9f54c65a0cd207a9f15d951e4ba9017d3a' 
+    const program = {
+      script: s,
+      language: l,
+      versionIndex: vI,
+      clientId: cI,
+      clientSecret: cS
+    };
+    request({
+      url: 'https://api.jdoodle.com/execute',
+      method: 'POST',
+      json: program
+    },
+    function (error, response, body) {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      console.log('body:', body);
+    });
+  }
+  
+function isError(body) {
+    if (body.slice(0, 10) === '\nTraceback') {
+      return true;
+    }
+    return false;
+}
